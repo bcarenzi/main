0b4b2763a9c42b423a80bc2e9c6eaacf7d4939d9
brunacarenzi <brunacarenzi@gmail.com>
2025-11-29 08:02:15 -0300

    test: edit step definition to be equal to feature files

--- api_tests/features/step_definitions/check_character.rb
+++ api_tests/features/step_definitions/check_character.rb
@@ -1,13 +1,13 @@
 require 'httparty'
 require 'json'
 
-Given('I search for the first character in the Rick and Morty API') do
+Given('I search for the first character into Rick and Morty API') do
   # Doing the request to get the details of the first character (ID 1)
   @response = HTTParty.get('https://rickandmortyapi.com/api/character/1')
   puts @response.body
 end
 
-When('I retrieve the character details from the response') do
+When('I got the result from the request') do
   # Checking if the request was successful (code 200)
   expect(@response.code).to eql 200
 
@@ -19,17 +19,29 @@ When('I retrieve the character details from the response') do
   @character_status = @character_data['status']
 end
 
-Then('I should see the character name as {string}') do |expected_name|
-  # Checking if the character name is the expected
-  expect(@character_name).to eql(expected_name)
+Then('I can see the character name') do
+  # Checking if the character name exists and is not empty
+  expect(@character_name).not_to be_nil
+  expect(@character_name).not_to be_empty
 
   # Displaying the character name in the console for debugging purposes
   puts "Character name: #{@character_name}"
 end
 
-Then('the character status should not be {string}') do |unexpected_status|
-  # Checking if the character status is not the unexpected value
-  expect(@character_status).not_to eql(unexpected_status)
+When('I got the status') do
+  # Checking if the request was successful (code 200)
+  expect(@response.code).to eql 200
+
+  # Parsing the response body to access the data
+  @character_data = JSON.parse(@response.body) unless @character_data
+
+  # Storing the character status
+  @character_status = @character_data['status']
+end
+
+Then('the status should not be dead') do
+  # Checking if the character status is not "Dead"
+  expect(@character_status).not_to eql('Dead')
 
   # Displaying the character status in the console for debugging purposes
   puts "Character status: #{@character_status}"
--- api_tests/features/step_definitions/check_location.rb
+++ api_tests/features/step_definitions/check_location.rb
@@ -1,13 +1,13 @@
 require 'httparty'
 require 'json'
 
-Given('I search for Rick Sanchez in the Rick and Morty API') do
+Given('I search Rick location into Rick and Morty API') do
   # Doing the request to get the details of the character Rick Sanchez (ID 1)
   @response = HTTParty.get('https://rickandmortyapi.com/api/character/1')
   puts @response.body
 end
 
-When('I retrieve the location information from the response') do
+When('I got the location from the request') do
   # Checking if the request was successful (code 200)
   expect(@response.code).to eql 200
 
@@ -18,9 +18,10 @@ When('I retrieve the location information from the response') do
   @character_location = @character_data['location']['name']
 end
 
-Then('I should see the correct location name') do
-  # Checking if the returned location is the expected
-  expect(@character_location).to eql('Citadel of Ricks')
+Then('I can see the location name') do
+  # Checking if the location name exists and is not empty
+  expect(@character_location).not_to be_nil
+  expect(@character_location).not_to be_empty
 
   # Displaying the location in the console for debugging purposes
   puts "Rick Sanchez's location: #{@character_location}"
